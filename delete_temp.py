59bb161a5e11b63618dbc248a4a24aac22b54d04
DuenoHfao <48118994+DuenoHfao@users.noreply.github.com>
2024-08-06 08:35:38 +0800

    Update delete_temp.py

--- delete_temp.py
+++ delete_temp.py
@@ -1,14 +1,14 @@
 import os
 
 def clear_temp():
-    temp_dest = "C:/Users/DuenoHfao/Desktop/Programming/Download YouTube/"
+    temp_dest = "" # home folder
     files = os.listdir(temp_dest)
     for i in files:
         if i[:-4] == ".mp4" or i[:-4] == ".mp3":
             file_path = temp_dest + i
             os.remove(file_path)
 
-    temp_dest += "Temp/"
+    temp_dest += "temp/"
     temp_files = os.listdir(temp_dest)
     for i in temp_files:
         file_path = temp_dest + i
